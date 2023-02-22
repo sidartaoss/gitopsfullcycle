c798203be8c37d7530c3a1d99fa4e9049c8b054b
sidartaoss <semprebono@gmail.com>
2023-02-22 16:48:09 -0300

    add: write hello argo

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Hello world</h1>"))
+		w.Write([]byte("<h1>Hello Argo</h1>"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
